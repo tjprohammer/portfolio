48f7442ccd6d9b39bc944ce6422a47e6e4458333
tjprohammer <prohammer1@gmail.com>
2022-12-07 20:39:25 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const App = () => {
+  return (
+    <h1>
+      TJ Prohammer Portfolio
+      <Button variant="text">Text</Button>
+    </h1>
+  );
+};
+
+export default App;
