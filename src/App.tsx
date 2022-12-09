646b7191f8dc4836a422c03dae35835ee60d6a10
tjprohammer <prohammer1@gmail.com>
2022-12-08 20:41:21 -0800

    setting up context theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-
-import ToggleColorMode from './Context/ThemeContext'
-import React from 'react'
-import LandingPage from './Pages/LandingPage'
- './Pages/LandingPage'
-
+import { ThemeProvider } from '@mui/material/styles';
+import { darkTheme } from './Context/themeColors';
+import React from 'react';
+import LandingPage from './Pages/LandingPage';
+('./Pages/LandingPage');
 
 const App = () => {
-  return (
-    <>
-      <ToggleColorMode />
-        <LandingPage />
-
-    </>
-  )
-}
-
-export default App
+    return (
+        <>
+            <ThemeProvider theme={darkTheme}>
+                <LandingPage />
+            </ThemeProvider>
+        </>
+    );
+};
+export default App;
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button, Card } from '@mui/material';
+import React from 'react';
 
 function LandingPage() {
-  return (
-    <div>LandingPage</div>
-  )
+    return (
+        <>
+            <Card color="primary" sx={{ mt: 10, p: 9, pt: 2 }}>
+                <h1 color="primary">TJ Prohammer Portfolio</h1>
+                <h2 color="secondary">Resume Attached</h2>
+                <h5 color="secondary">Testing</h5>
+                <Button color="primary" variant="contained">
+                    LandingPage
+                </Button>
+                <Button color="warning" variant="contained">
+                    LandingPage
+                </Button>
+                <Button color="secondary" variant="contained">
+                    LandingPage
+                </Button>
+                <Button color="error" variant="contained">
+                    LandingPage
+                </Button>
+                <Button color="success" variant="contained">
+                    LandingPage
+                </Button>
+            </Card>
+        </>
+    );
 }
 
-export default LandingPage
+export default LandingPage;
