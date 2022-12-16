1f9e1b522644fa44e2c4cb73b339c902fd1fefae
tjprohammer <prohammer1@gmail.com>
2022-12-16 12:50:49 -0800

    updates

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+// import SvgAnimation from '../components/SVGAnimation/SvgAnimation';
+
+
 
 function LandingPage() {
     return (
         <>
             <Box
+            
                 sx={{
+                    height: '88vh',
                     display: 'grid',
                     gridTemplateColumns: 'repeat(2, 1fr)',
                     gap: '2',
-                    columnGap: '3'
+                    columnGap: '3',
+                    alignItems: 'center'
                 }}>
                 <Typography
                     variant="h4"
@@ -17,12 +23,9 @@ function LandingPage() {
                     sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     TJ Prohammer Portfolio
                 </Typography>
-                <Typography
-                    variant="h4"
-                    color="primary.dark"
-                    sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    TJ Prohammer Portfolio
-                </Typography>
+                {/* <SvgAnimation /> */}
+             
+
             </Box>
         </>
     );
