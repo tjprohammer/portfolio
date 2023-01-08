0e73e374e43d9c7aff420e2a1fed216485b0346f
tjprohammer <prohammer1@gmail.com>
2023-01-08 15:30:45 -0800

    updates

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../../assets/logo-svg.svg';
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Grid, IconButton, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 
 import { motion, Variants } from 'framer-motion';
+import { NavLink } from 'react-router-dom';
 
 const itemVariants: Variants = {
     open: {
@@ -14,7 +15,7 @@ const itemVariants: Variants = {
     closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
 };
 
-function LandingPage() {
+function LandingPage(props) {
     return (
         <Container
             sx={{
@@ -136,6 +137,30 @@ function LandingPage() {
                         variants={itemVariants}
                     />
                 </Grid>
+                <motion.div
+                    whileHover={{ scale: 1.2 }}
+                    initial={{ opacity: 0, y: 300 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        type: 'spring',
+                        delay: 0.3,
+                        duration: 2,
+                        bounce: 0.6
+                    }}
+                    variants={itemVariants}>
+                    <IconButton>
+                        <NavLink to='/about'>
+                            <svg
+                                fill="teal"
+                                viewBox="0 0 16 16"
+                                height="3em"
+                                width="3em"
+                                {...props}>
+                                <path d="M8 0a8 8 0 110 16A8 8 0 018 0zM4.5 7.5a.5.5 0 000 1h5.793l-2.147 2.146a.5.5 0 00.708.708l3-3a.5.5 0 000-.708l-3-3a.5.5 0 10-.708.708L10.293 7.5H4.5z" />
+                            </svg>
+                        </NavLink>
+                    </IconButton>
+                </motion.div>
             </Grid>
         </Container>
     );
